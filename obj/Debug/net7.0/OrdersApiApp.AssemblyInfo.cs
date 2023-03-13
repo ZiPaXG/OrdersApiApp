0c86f33f06bada1c131663fa014db182269de336
ZiPaXG <shepelev.danilka@bk.ru>
2023-03-13 19:07:58 +0300

    Add Dockerfile

--- obj/Debug/net7.0/OrdersApiApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/OrdersApiApp.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("9745c75f-303e-4d51-bf67-db5e5f292fb6")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrdersApiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
